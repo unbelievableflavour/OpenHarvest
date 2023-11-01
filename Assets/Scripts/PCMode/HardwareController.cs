bdfee1e41380d664772ae1cbeca01be96bc863ca
b <b>
2023-11-01 11:17:30 +0100

    Moved PC controller out of _Core (Instantiate when needed)

--- Assets/Scripts/PCMode/HardwareController.cs
+++ Assets/Scripts/PCMode/HardwareController.cs
@@ -17,8 +17,8 @@ public class HardwareController : MonoBehaviour
         {
             GetComponent<HeadsetDetector>().enabled = false;
             VRPlayerObject.SetActive(false);
-            PCPlayerObject.SetActive(true);
-        }        
+            Instantiate(PCPlayerObject);
+        }
     }
 #endif
 }
