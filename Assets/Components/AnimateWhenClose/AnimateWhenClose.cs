d39ada497d0100a49cafb5531853527cae7e723c
b <b>
2023-11-04 13:57:57 +0100

    Disable animations when far away

---
+++ Assets/Components/AnimateWhenClose/AnimateWhenClose.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateWhenClose : MonoBehaviour
+{
+    public List<Animator> animators = new List<Animator>();
+    public int maxShowDistance = 10;
+
+    Transform mainCam;
+
+    bool isEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main.transform;
+        DisableAnimators();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Show if within range
+        float currentDistance = Vector3.Distance(transform.position, mainCam.position);
+        if(currentDistance > maxShowDistance) {
+            DisableAnimators();
+            return;
+        }
+
+        EnableAnimators();
+    }
+
+    void DisableAnimators() {
+        if(!isEnabled) {
+            return;
+        }
+
+        isEnabled = false;
+        foreach(Animator animator in animators) {
+            animator.enabled = false;
+        }
+    }
+
+    void EnableAnimators() {
+        if(isEnabled) {
+            return;
+        }
+
+        Debug.Log("enable!");
+
+        isEnabled = true;
+        foreach(Animator animator in animators) {
+            animator.enabled = true;
+        }
+    }
+}
