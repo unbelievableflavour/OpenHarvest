fe9e3599d5c797034c4375c69c9de0453b7ecf46
b <b>
2023-11-04 12:01:42 +0100

    Optimized tooltip to only be needed twice in the scene
    Potentially fixed hand fall bug on pickaxe

---
+++ Assets/Components/Player/ItemInspector/ItemInspector.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+
+public class ItemInspector : MonoBehaviour {
+    public Vector3 TipOffset = new Vector3(1.5f, 0.2f, 0);
+    public bool UseWorldYAxis = true;
+    public float MaxViewDistance = 10f;
+
+    public LineToTransform lineToTransform;
+    
+    [Header("Filled during play")]
+    public Transform DrawLineTo;
+
+    /// <summary>
+    /// Hide this if farther than MaxViewDistance
+    /// </summary>
+    Transform childTransform;
+    Transform lookAt;
+    
+    private bool shouldUpdate = false;
+    private Transform originalParent;
+
+    void Start() {
+        originalParent = transform.parent;
+        lookAt = Camera.main.transform;
+        childTransform = transform.GetChild(0);
+    }
+
+    public void Init(Transform transform) {
+        DrawLineTo = transform;
+        lineToTransform.ConnectTo = transform;
+    }
+
+    public void StartUpdating() {
+        shouldUpdate = true;
+        transform.SetParent(DrawLineTo, false);
+    }
+
+    public void StopUpdating() {
+        shouldUpdate = false;
+        transform.SetParent(originalParent, false);
+    }
+
+    void Update() {
+        if(!shouldUpdate) {
+            return;
+        }
+
+        UpdateTooltipPosition();
+    }
+
+    public virtual void UpdateTooltipPosition() {
+        if (lookAt) {
+            transform.LookAt(Camera.main.transform);
+        }
+        else if (Camera.main != null) {
+            lookAt = Camera.main.transform;
+        }
+        else if (Camera.main == null) {
+            return;
+        }
+
+        transform.localPosition = TipOffset;
+
+        if (UseWorldYAxis) {
+            transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+            transform.position += new Vector3(0, TipOffset.y, 0);
+        }
+
+        if (childTransform) {
+            childTransform.gameObject.SetActive(Vector3.Distance(transform.position, Camera.main.transform.position) <= MaxViewDistance);
+        }
+    }
+}
---
+++ Assets/Components/Player/ItemInspector/ItemInspectorFader.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BNG;
+
+public class ItemInspectorFader : MonoBehaviour
+{
+    public List<ControllerBinding> Input = new List<ControllerBinding>() { ControllerBinding.None };
+    public Text tooltipText;
+    public GameObject tooltipLine;
+    public Canvas canvas;    
+    public ItemInspector tooltip;
+
+    [Header("Filled during play")]
+    public Grabber grabber;
+
+    Grabbable grabbable;
+
+    bool tooltipCurrentlyShown = false;
+    HarvestDataTypes.Item item;
+    
+    void Start()
+    {
+        canvas.enabled = false;
+        tooltipLine.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (!ifKeyDown()) {
+            FadeTooltip(false);
+            return;
+        }
+
+        if (grabber.HeldGrabbable == null) {
+            FadeTooltip(false);
+            return;
+        }
+
+        if(tooltip.DrawLineTo != grabber.HeldGrabbable.transform) {
+            Init(grabber.HeldGrabbable);
+        }
+
+        FadeTooltip(true);
+    }
+
+    public void Init(Grabbable heldGrabbable) {
+        //if it is a subgrab, grab the parent grab.
+        item = heldGrabbable.OtherGrabbableMustBeGrabbed 
+        ? Definitions.GetItemFromObject(heldGrabbable.OtherGrabbableMustBeGrabbed) : Definitions.GetItemFromObject(heldGrabbable);
+
+        //if no item can be found.
+        if (item == null) {
+            return;
+        }
+
+        tooltip.Init(heldGrabbable.transform);
+        tooltipText.text = item.name + "\n\n" + item.description;
+    }
+
+    public void FadeTooltip(bool showTooltip) {
+        if(showTooltip == tooltipCurrentlyShown) {
+            return;
+        }
+
+        if (item == null) {
+            return;
+        }
+
+        // Animate ring opacity in / out
+        if (showTooltip)
+        {
+            tooltip.StartUpdating();
+            tooltipCurrentlyShown = true;
+            tooltipLine.SetActive(true);
+            canvas.enabled = true;
+            return;
+        }
+        
+        tooltipCurrentlyShown = false;
+        tooltip.StopUpdating();
+        canvas.enabled = false;
+        tooltipLine.SetActive(false);
+        return;
+    }
+
+    public virtual bool ifKeyDown()
+    {
+        // Check for bound controller button
+        for (int x = 0; x < Input.Count; x++)
+        {
+            if (InputBridge.Instance.GetControllerBindingValue(Input[x]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
