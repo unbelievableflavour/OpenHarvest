6a1832da4227ec87b26ebf89f621dfb3599ce51f
b <b>
2023-11-04 12:23:05 +0100

    Added Harvest settings to menu bar in unity editor

--- Assets/ScriptableObjects/HarvestSettings.cs
+++ Assets/ScriptableObjects/HarvestSettings.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class HarvestSettings : ScriptableObject
 {
     public bool isPCMode = false;
@@ -15,4 +19,12 @@ public class HarvestSettings : ScriptableObject
     public bool showAllModulesOnStart = false;
     public bool showOverlapColliders = false;
     public bool showRemovedOverlapColliders = false;
+
+#if UNITY_EDITOR
+    [MenuItem("Harvest VR/Settings")]
+    private static void OpenSettingsSO() {
+        var texturePath = AssetDatabase.LoadMainAssetAtPath($"Assets/ScriptableObjects/HarvestSettings.asset");
+        AssetDatabase.OpenAsset(texturePath);
+    }
+#endif
 }
