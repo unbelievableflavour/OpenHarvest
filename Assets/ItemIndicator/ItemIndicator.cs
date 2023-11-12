0ab81ad6553b164758fefcef2b230482aff59f42
b <b>
2023-11-12 01:05:57 +0100

    Disable ItemIndicator in snap zones

---
+++ Assets/ItemIndicator/ItemIndicator.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// GameObjects with this component will be disabled in snap zones.
+public class ItemIndicator : MonoBehaviour {}
