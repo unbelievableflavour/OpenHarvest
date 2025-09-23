08763669856b2eb1a736fdb20d416c41950d6f10
bart <bart.zaalberg@shift2.nl>
2025-09-23 15:51:12 +0200

    Fixed resolution scale and shadow settings + added more scale options

--- Assets/Scripts/PlayerCustomSettings.cs
+++ Assets/Scripts/PlayerCustomSettings.cs
@@ -2,6 +2,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal; // Add this back
 
 public class PlayerCustomSettings : MonoBehaviour
 {
@@ -17,6 +18,9 @@ public class PlayerCustomSettings : MonoBehaviour
     public event EventHandler assistModeToggled;
 
     private int defaultShadowDistanceValue = 10;
+
+    private UniversalRenderPipelineAsset clonedPipeline;
+    private bool pipelineCloned = false;
 
     // Initialize the instance.
     private void Awake()
@@ -37,51 +41,51 @@ public class PlayerCustomSettings : MonoBehaviour
         assistModeToggled?.Invoke(this, null);
     }
 
+    private void EnsureClonedPipeline()
+    {
+        if (!pipelineCloned)
+        {
+            var originalPipeline = QualitySettings.renderPipeline as UniversalRenderPipelineAsset;
+            if (originalPipeline != null)
+            {
+                clonedPipeline = Instantiate(originalPipeline);
+                QualitySettings.renderPipeline = clonedPipeline;
+                GraphicsSettings.defaultRenderPipeline = clonedPipeline;
+                pipelineCloned = true;
+                Debug.Log("Created cloned URP pipeline for runtime modifications");
+            }
+        }
+    }
+
     public void RefreshSettings()
     {
         handModelSelector.ChangeHandsModel(bool.Parse(GameState.Instance.settings["useHandsOnly"]) ? 9 : 8, true);
         locomotionManager.ChangeLocomotion(bool.Parse(GameState.Instance.settings["useSmoothLocomotion"]) ? LocomotionType.SmoothLocomotion : LocomotionType.Teleport, true);
-        playerRotation.RotationType = bool.Parse(GameState.Instance.settings["useSmoothTurning"]) ? RotationMechanic.Smooth : RotationMechanic.Snap; ;
+        playerRotation.RotationType = bool.Parse(GameState.Instance.settings["useSmoothTurning"]) ? RotationMechanic.Smooth : RotationMechanic.Snap;
         playerRotation.SmoothTurnSpeed = float.Parse(GameState.Instance.settings["smoothTurningSensitivity"]) * 20;
         BNGPlayerOffset.CharacterControllerYOffset = float.Parse(GameState.Instance.settings["playerHeightOffset"]) / 10;
         SetUseAssistMode();
         AudioManager.Instance.ChangeMusicVolume(float.Parse(GameState.Instance.settings["backgroundMusicVolume"]) / 10);
         playerColours.Refresh();
 
-        // Refresh rate setting removed - was Oculus-specific
-        // var refreshRate = getRefreshRate();
-        // if (Unity.XR.Oculus.Performance.TryGetDisplayRefreshRate(out var currentRefreshRate)) {
-        //     if (currentRefreshRate != refreshRate) {
-        //         Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(refreshRate);
-        //     }
-        // }
+        // Ensure we have a cloned pipeline for modifications
+        EnsureClonedPipeline();
 
-        var pipeline = GraphicsSettings.defaultRenderPipeline;
+        var refreshRate = getRefreshRate();
+        Application.targetFrameRate = (int)refreshRate;
+        Debug.Log($"Set refresh rate to {refreshRate}Hz");
+
         var resolutionScale = getResolutionScale();
-        // Note: renderScale property may not be available in all render pipelines
-        // This was specific to Universal Render Pipeline
-        // if (pipeline.renderScale != resolutionScale) {       
-        //     pipeline.renderScale = resolutionScale;
-        // }
+        clonedPipeline.renderScale = resolutionScale;
 
         bool useShadows = bool.Parse(GameState.Instance.settings["useShadows"]);
-        if (isShadowsActive(pipeline) != useShadows)
-        {
-            QualitySettings.shadowDistance = useShadows ? defaultShadowDistanceValue : 0;
-        }
+        clonedPipeline.shadowDistance = useShadows ? defaultShadowDistanceValue : 0;
 
         bool useFog = bool.Parse(GameState.Instance.settings["useFog"]);
         if (RenderSettings.fog != useFog)
         {
             RenderSettings.fog = useFog;
         }
-    }
-
-    public bool isShadowsActive(RenderPipelineAsset pipeline)
-    {
-        // Default behavior for shadow checking
-        // This was specific to Universal Render Pipeline
-        return QualitySettings.shadowDistance > 0;
     }
 
     public float getRefreshRate()
@@ -108,22 +112,37 @@ public class PlayerCustomSettings : MonoBehaviour
     {
         if(GameState.Instance.settings["resolutionScale"] == "0")
         {
+            return 0.5f;
+        }
+
+        if(GameState.Instance.settings["resolutionScale"] == "1")
+        {
+            return 0.75f;
+        }
+
+        if(GameState.Instance.settings["resolutionScale"] == "2")
+        {
             return 1f;
-        }
-
-        if (GameState.Instance.settings["resolutionScale"] == "1")
-        {
-            return 1.25f;
-        }
-
-        if (GameState.Instance.settings["resolutionScale"] == "2")
-        {
-            return 1.4f;
         }
 
         if (GameState.Instance.settings["resolutionScale"] == "3")
         {
+            return 1.25f;
+        }
+
+        if (GameState.Instance.settings["resolutionScale"] == "5")
+        {
             return 1.5f;
+        }
+
+        if (GameState.Instance.settings["resolutionScale"] == "6")
+        {
+            return 1.75f;
+        }
+
+        if (GameState.Instance.settings["resolutionScale"] == "7")
+        {
+            return 2f;
         }
 
         return 1f;
